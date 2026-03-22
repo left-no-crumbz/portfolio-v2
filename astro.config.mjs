ab39c45cf467013aafbb86f315b7598f371f093d
crumbz <133969306+left-no-crumbz@users.noreply.github.com>
2026-03-22 14:56:52 +0800

    Add @playform/inline integration and reformat astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,17 +3,21 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import playformInline from '@playform/inline';
+
 // https://astro.build/config
 export default defineConfig({
-    experimental: {
-        queuedRendering: {
-            enabled: true,
-            poolSize: 100,
-            contentCache: true
-        }
-    },
+  experimental: {
+      queuedRendering: {
+          enabled: true,
+          poolSize: 100,
+          contentCache: true
+      }
+  },
 
-    vite: {
-        plugins: [tailwindcss()]
-    }
+  vite: {
+      plugins: [tailwindcss()]
+  },
+
+  integrations: [playformInline()]
 });
